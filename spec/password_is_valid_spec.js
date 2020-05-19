6197facfc2063066178160c101237bad4abab4e9
Thabiso Makolana <makolanathabiso@gmail.com>
2020-05-19 14:09:55 +0200

    final product i think

--- spec/password_is_valid_spec.js
+++ spec/password_is_valid_spec.js
@@ -1,34 +1,29 @@
 let {passwordIsValid} = require("../src/password_checker.js")
 
 describe("check if the password meets a few different conditions",()=>{
-    const existRegex = /Password12@|pass|PASSWORD/
-    const lowercaseRegex = /[a-z]+/
-    const uppercaseRegex = /[a-z]+/
-    const numberRegex = /[0-9]+/
-    const specialCaseRegex = /[^0-9a-zA-Z]+/
 
     it("password should exist",()=>{
-        expect(existRegex.test("Password12@")).toBe(true)
+        expect(()=>{passwordIsValid(" ")}).toThrow(new Error("password should exist"))
     })
 
     it("password should be longer than than 8 characters",()=>{
-        expect(passwordIsValid("Password12@")).toBe(null)
+        expect(()=>{passwordIsValid("webdev")}).toThrow(new Error("password should be longer than than 8 characters"))
     })
 
     it("password should have at least one lowercase letter",()=>{
-        expect(lowercaseRegex.test("Password12@")).toBe(true)
+        expect(()=>{passwordIsValid("PASSWORD12@")}).toThrow(new Error("password should have at least one lowecase letter"))
     })
 
     it("password should have at least one uppercase letter",()=>{
-        expect(uppercaseRegex.test("Password12@")).toBe(true)
+        expect(()=>{passwordIsValid("password12@")}).toThrow(new Error("password should have at least one uppercase letter"))
     })
 
     it("password should have at least one number",()=>{
-        expect(numberRegex.test("Password12@")).toBe(true)
+        expect(()=>{passwordIsValid("Password@")}).toThrow(new Error("password should have at least one number"))
     })
 
     it("password should have at least one special character",()=>{
-        expect(specialCaseRegex.test("Password12@")).toBe(true)
+        expect(()=>{passwordIsValid("Password12")}).toThrow(new Error("password should have at least one special character"))
     })
 
 })
--- src/password_checker.js
+++ src/password_checker.js
@@ -1,15 +1,12 @@
-let existingPasswword = "Password12@"
-
 const passwordIsValid = password => {
 
-    const existRegex = /Password12@|pass|PASSWORD/
     const stringLength = password.length
     const lowercaseRegex = /[a-z]+/
     const uppercaseRegex = /[a-z]+/
     const numberRegex = /[0-9]+/
     const specialCaseRegex = /[^0-9a-zA-Z]+/
 
-    if (!existRegex.test(password)) throw "password should exist"
+    if (!password == " ") throw "password should exist"
 
     if (stringLength < 8) throw "password should be longer than than 8 characters"
 
@@ -25,14 +22,13 @@ const passwordIsValid = password => {
 const passwordIsOk = password => {
     
     let counter = 0
-    const existRegex = /Password12@|pass|PASSWOR|Password12/
     const stringLength = password.length
     const lowercaseRegex = /[a-z]+/
     const uppercaseRegex = /[a-z]+/
     const numberRegex = /[0-9]+/
     const specialCaseRegex = /[^0-9a-zA-Z]+/
 
-    if (!existRegex.test(password)) {
+    if (password == " ") {
         return false
     } else {
         counter++
